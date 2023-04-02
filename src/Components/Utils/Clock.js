f05583f4895e0221ed9d26a93abe2259bf2acbcb
TobyVB <98196156+TobyVB@users.noreply.github.com>
2023-04-02 22:18:42 +0800

    change clock

--- src/Components/Utils/Clock.js
+++ src/Components/Utils/Clock.js
@@ -52,9 +52,44 @@ export default function Clock(props) {
     }
   }
 
+  function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = seconds / 31536000;
+
+    if (interval > 1) {
+      return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 2) {
+      return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 604800;
+    if (interval > 1) {
+      return Math.floor(interval) + " weeks ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  }
+  var aDay = 24 * 60 * 60 * 1000;
+  console.log(timeSince(props.createdAt.toDate()));
+  console.log(timeSince(new Date(Date.now() - aDay * 2)));
+
   return (
     <div className={`time`}>
-      <p>{`${hour}:${minute}${timeOfDay}, ${day} ${month} ${year}`}</p>
+      {/* <p>{`${hour}:${minute}${timeOfDay}, ${day} ${month} ${year}`}</p> */}
+      <p>{timeSince(props.createdAt.toDate())}</p>
     </div>
   );
 }
